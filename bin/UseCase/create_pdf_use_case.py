331f8a39f85bcf40c8d8c2a6ae72dc06542cd689
SynerRaphael <raphael.biard@synerglass-soft.com>
2023-09-29 08:50:51 +0200

    creation Class
    Creation d'une classe python + amélioration de la création d'un PDF

--- bin/UseCase/create_pdf_use_case.py
+++ bin/UseCase/create_pdf_use_case.py
@@ -1,237 +1,80 @@
 from fpdf import FPDF
 import json
 
+class CreatePdfUseCase:
+    
+    def __init__(self, order):
+        self.order = order
+        self.currency = order["currency"]
+        self.pdf = FPDF('P', 'mm', 'Letter')
 
+
+    def create_pdf_fpdf(self):
+        self.pdf.add_page()
+        self.pdf.set_font('arial', '', 16)
+
+        # self.pdf.set_fill_color(200,100,58)
+        self.add_line("company", self.get_data("company"))
+        self.add_line("customer", self.get_data("customer"))
+        self.add_line("billing address", self.get_data("billing_address"))
+        self.add_line("delivery address", self.get_data("delivery_address"))
+        self.add_line("desired delivery", self.get_data("desired_delivery"))
+        self.add_line("agreed delivery", self.get_data("agreed_delivery"))
+        
+        for offer_line in self.order["offer_lines"]:
+            self.add_line_empty()
+            self.add_line("quantity", self.get_sub_data(offer_line, "quantity"))
+            self.add_line("product", self.get_sub_data(offer_line, "product"))
+            self.add_line("unit price", self.get_sub_data(offer_line, "unit_price"),self.currency)
+            self.add_line("orderline price", self.get_sub_data(offer_line, "orderline_price"),self.currency)
+        
+        self.add_line_empty()
+        self.add_line("total", self.get_data("total"), self.currency)
+        
+    def add_line(self, title, data, currency = ""):
+        self.pdf.cell(100, 10, title+" : "+str(data)+currency, 1, 1, 'L', fill=False)
+    
+    def add_line_empty(self):
+        self.pdf.cell(100, 10,"", 1, 1, 'L', fill=False)
+    
+    def get_data(self, key):
+        return self.order[key]
+    
+    def get_sub_data(self, tab, key):
+        return tab[key]
+    
+    def save_file(self, path):
+        
+        self.pdf.output(path)
+        
 def get_order():
-    return """{
-    "uuid": "0bada7889b4ec72b57c0436cef6fba1296520c6fe756595091d50e5b9eed7d6c",
-    "company_name":"Synerglass-Soft",
-    "internal_reference": "OF: 2022-13",
-    "customer_reference": "Customer ref 13",
-    "wished_delivery_date": "2023-07-17",
-    "commentary": "commentary offer 13",
-    "default_margin": 80,
-    "additional_margin": 14.42,
-    "delivery_address_uuid": "595b5a5bbd9b98da5292884c49e39ba6ae42da6cb69f1a7e9d2b65cc5e586f2c",
-    "offer_lines": [
+        return """{
+    "company":"Ma super companie",
+    "customer":"Mon super customer",
+    "billing_address":"Rue du moulin 24",
+    "delivery_address":"Rue de la loi 12",
+    "desired_delivery":"07/07/2024",
+    "agreed_delivery":"02/12/2025",
+    "currency":"euro",
+    "lang":"FR",
+    "offer_lines":[
         {
-            "uuid": "560ae92ce14470269979cfe140ed7aebb967cea0c1745fc119373466237adb9b",
-            "line_number": 1,
-            "quantity": 6,
-            "chassis_marker": "chassis_marker_1",
-            "width": 1000,
-            "height": 1000,
-            "item_description": "Description for line 1",
-            "surface": 1,
-            "reference": "reference for line 1",
-            "price_detail": {
-                "purchase_price": 440,
-                "selling_price_excluding_vat": 181,
-                "amount_vat": 34,
-                "selling_price_including_vat": 110
-            },
-            "catalogue_uuid": "37522f729a0e1b836888fd03cc0f09ddc148ae9705d87758857b6df09bbe8c34",
-            "item_uuid": "21cbb893282831e11f87a9033858687ba2f67691f86c1551d091c69ccffa855b",
-            "type_uuid": "0000000000000000000000000000000000000000000000000000000000000000"
+            "quantity":6,
+            "product":"product name 1",
+            "unit_price":1,
+            "orderline_price":6
         },
         {
-            "uuid": "aa23e471b4700947c2188900ba15a0540dadc195c20b0458c7d567f7963b79e3",
-            "line_number": 2,
-            "quantity": 6,
-            "chassis_marker": "",
-            "width": 0,
-            "height": 0,
-            "item_description": "Description for line 2",
-            "surface": 0,
-            "reference": "reference for line 2",
-            "price_detail": {
-                "purchase_price": 484,
-                "selling_price_excluding_vat": 42,
-                "amount_vat": 10,
-                "selling_price_including_vat": 26
-            },
-            "catalogue_uuid": "055823e65a5291d33014d3b40875c2007fd0ab98b8fe8af404cb79185d23e623",
-            "item_uuid": "8849ccc958f19987a5987b63231810445839e6e58daa62dbc261747dbf676b64",
-            "type_uuid": "0000000000000000000000000000000000000000000000000000000000000000"
-        },
-        {
-            "uuid": "63ae512e3b2948d3d25348773124f3e7b18bc6031f970800c2cec2db008ac1f9",
-            "line_number": 3,
-            "quantity": 9,
-            "chassis_marker": "",
-            "width": 0,
-            "height": 0,
-            "item_description": "Description for line 3",
-            "surface": 0,
-            "reference": "reference for line 3",
-            "price_detail": {
-                "purchase_price": 117,
-                "selling_price_excluding_vat": 194,
-                "amount_vat": 49,
-                "selling_price_including_vat": 421
-            },
-            "catalogue_uuid": "0000000000000000000000000000000000000000000000000000000000000000",
-            "item_uuid": "0000000000000000000000000000000000000000000000000000000000000000",
-            "type_uuid": "0000000000000000000000000000000000000000000000000000000000000000"
-        },
-        {
-            "uuid": "ce0d53272392e8e41214d253f26a62fa0acb3e6cf1bd0029b13d90fde7041129",
-            "line_number": 4,
-            "quantity": 9,
-            "chassis_marker": "chassis_marker_4",
-            "width": 4000,
-            "height": 4000,
-            "item_description": "Description for line 4",
-            "surface": 16,
-            "reference": "reference for line 4",
-            "price_detail": {
-                "purchase_price": 265,
-                "selling_price_excluding_vat": 342,
-                "amount_vat": 33,
-                "selling_price_including_vat": 246
-            },
-            "catalogue_uuid": "37522f729a0e1b836888fd03cc0f09ddc148ae9705d87758857b6df09bbe8c34",
-            "item_uuid": "21cbb893282831e11f87a9033858687ba2f67691f86c1551d091c69ccffa855b",
-            "type_uuid": "0000000000000000000000000000000000000000000000000000000000000000"
-        },
-        {
-            "uuid": "7d7081f7f3268368d4303366e394118475e4e5d480bc30dd540c22c68d92de95",
-            "line_number": 6,
-            "quantity": 2,
-            "chassis_marker": "",
-            "width": 0,
-            "height": 0,
-            "item_description": "Description for line 6",
-            "surface": 0,
-            "reference": "reference for line 6",
-            "price_detail": {
-                "purchase_price": 354,
-                "selling_price_excluding_vat": 226,
-                "amount_vat": 30,
-                "selling_price_including_vat": 131
-            },
-            "catalogue_uuid": "0000000000000000000000000000000000000000000000000000000000000000",
-            "item_uuid": "0000000000000000000000000000000000000000000000000000000000000000",
-            "type_uuid": "0000000000000000000000000000000000000000000000000000000000000000"
-        },
-        {
-            "uuid": "9ccc59b98aa3216ffda6b33bec9676e0bb1923c7395a912d267fe64b4450bc74",
-            "line_number": 7,
-            "quantity": 3,
-            "chassis_marker": "chassis_marker_7",
-            "width": 7000,
-            "height": 7000,
-            "item_description": "Description for line 7",
-            "surface": 49,
-            "reference": "reference for line 7",
-            "price_detail": {
-                "purchase_price": 425,
-                "selling_price_excluding_vat": 106,
-                "amount_vat": 35,
-                "selling_price_including_vat": 487
-            },
-            "catalogue_uuid": "37522f729a0e1b836888fd03cc0f09ddc148ae9705d87758857b6df09bbe8c34",
-            "item_uuid": "21cbb893282831e11f87a9033858687ba2f67691f86c1551d091c69ccffa855b",
-            "type_uuid": "0000000000000000000000000000000000000000000000000000000000000000"
-        },
-        {
-            "uuid": "e12db18b353393efa3dd197c88d825b4b5a2f015e8ecda8f182f4ce1dc2d82c9",
-            "line_number": 8,
-            "quantity": 4,
-            "chassis_marker": "",
-            "width": 0,
-            "height": 0,
-            "item_description": "Description for line 8",
-            "surface": 0,
-            "reference": "reference for line 8",
-            "price_detail": {
-                "purchase_price": 489,
-                "selling_price_excluding_vat": 416,
-                "amount_vat": 54,
-                "selling_price_including_vat": 128
-            },
-            "catalogue_uuid": "055823e65a5291d33014d3b40875c2007fd0ab98b8fe8af404cb79185d23e623",
-            "item_uuid": "8849ccc958f19987a5987b63231810445839e6e58daa62dbc261747dbf676b64",
-            "type_uuid": "0000000000000000000000000000000000000000000000000000000000000000"
-        },
-        {
-            "uuid": "a60c31149299f4fa82136934e722f91512d8c2bc7705fcdbb1f89e58e803c59a",
-            "line_number": 9,
-            "quantity": 2,
-            "chassis_marker": "",
-            "width": 0,
-            "height": 0,
-            "item_description": "Description for line 9",
-            "surface": 0,
-            "reference": "reference for line 9",
-            "price_detail": {
-                "purchase_price": 490,
-                "selling_price_excluding_vat": 235,
-                "amount_vat": 77,
-                "selling_price_including_vat": 316
-            },
-            "catalogue_uuid": "0000000000000000000000000000000000000000000000000000000000000000",
-            "item_uuid": "0000000000000000000000000000000000000000000000000000000000000000",
-            "type_uuid": "0000000000000000000000000000000000000000000000000000000000000000"
+            "quantity":2,
+            "product":"product name 2",
+            "unit_price":2.5,
+            "orderline_price":5
         }
     ],
-    "totals": {
-        "quantity": 54,
-        "surface": 54,
-        "amout_energetic_surcharge": 54,
-        "amount_excluding_vat": 54,
-        "amount_vat": 54,
-        "amount_including_vat": 54
-    }
-}"""
-
-
-def create_pdf_fpdf(order):
-    pdf = FPDF('P', 'mm', 'Letter')
-
-    pdf.add_page()
-
-    pdf.set_font('helvetica', '', 16)
-
-    # pdf.set_fill_color(200,100,58)
-
-    pdf.cell(100, 10, 'company_name : ' +
-             order["company_name"], 1, 1, 'L', fill=False)
-    pdf.cell(100, 10, 'internal_reference : ' +
-             order["internal_reference"], 1, 1, 'L', fill=False)
-    pdf.cell(100, 10, 'customer_reference : ' +
-             order["customer_reference"], 1, 1, 'L', fill=False)
-    pdf.cell(100, 10, 'wished_delivery_date : ' +
-             order["wished_delivery_date"], 1, 1, 'L', fill=False)
-    pdf.cell(100, 10, 'commentary : ' +
-             order["commentary"], 1, 1, 'L', fill=False)
-    pdf.cell(100, 10, 'default_margin : ' +
-             str(order["default_margin"]), 1, 1, 'L', fill=False)
-    pdf.cell(100, 10, 'additional_margin : ' +
-             str(order["additional_margin"]), 1, 1, 'L', fill=False)
-
-    pdf.cell(100, 10, '', 1, 1, 'L', fill=False)
-
-    pdf.cell(100, 10, 'Offer_lines : ', 1, 1, 'L', fill=False)
-
-    pdf.cell(100, 10, '', 1, 1, 'L', fill=False)
-
-    pdf.cell(100, 10, 'totals : ', 1, 1, 'L', fill=False)
-    pdf.cell(100, 10, 'quantity : ' +
-             str(order["totals"]["quantity"]), 1, 1, 'L', fill=False)
-    pdf.cell(100, 10, 'surface : ' +
-             str(order["totals"]["surface"]), 1, 1, 'L', fill=False)
-    pdf.cell(100, 10, 'amout_energetic_surcharge : ' +
-             str(order["totals"]["amout_energetic_surcharge"]), 1, 1, 'L', fill=False)
-    pdf.cell(100, 10, 'amount_excluding_vat : ' +
-             str(order["totals"]["amount_excluding_vat"]), 1, 1, 'L', fill=False)
-    pdf.cell(100, 10, 'amount_vat : ' +
-             str(order["totals"]["amount_vat"]), 1, 1, 'L', fill=False)
-    pdf.cell(100, 10, 'amount_including_vat : ' +
-             str(order["totals"]["amount_including_vat"]), 1, 1, 'L', fill=False)
-
-    # with pdf.table() as table:
-    #     row = table.row
-    #     row.cell("Hello")
-
-    pdf.output('pdf_1.pdf')
+    "total":11
+    }"""
+    
+order = json.loads(get_order())
+pdf = CreatePdfUseCase(order)
+pdf.create_pdf_fpdf()
+pdf.save_file("pdf_1.pdf")
--- bin/app.py
+++ bin/app.py
@@ -1,17 +1,42 @@
 from flask import Flask, make_response
-import UseCase.create_pdf_use_case
+from UseCase.create_pdf_use_case import CreatePdfUseCase
 import json
 
 app = Flask(__name__)
 
+def get_order():
+        return """{
+    "company":"Ma super companie",
+    "customer":"Mon super customer",
+    "billing_address":"Rue du moulin 24",
+    "delivery_address":"Rue de la loi 12",
+    "desired_delivery":"07/07/2024",
+    "agreed_delivery":"02/12/2025",
+    "currency":"euro",
+    "offer_lines":[
+        {
+            "quantity":6,
+            "product":"product 1",
+            "unit_price":1,
+            "orderline_price":6
+        },
+        {
+            "quantity":2,
+            "product":"product 2",
+            "unit_price":2.5,
+            "orderline_price":5
+        }
+    ],
+    "total":11
+    }"""
 
 @app.get('/v1/document')
 def get_pdf():
-    test = '{ "Hello":"world " }'
-    # order = json.loads(UseCase.create_pdf_use_case.get_order())
-    # pdf = UseCase.create_pdf_use_case.create_pdf_fpdf(order)
-    # response = make_response(pdf)
-    return test
+    order = json.loads(get_order())
+    
+    pdf = CreatePdfUseCase(order)
+    response = make_response(pdf)
+    return response
 
 
 if __name__ == '__main__':
